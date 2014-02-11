b4b60c9ba70e22da8c30961be6367a05b55fb7d7
Alexandros Sigalas <alxarch@gmail.com>
2014-02-11 05:46:38 +0200

    Fixes argument parsing.

--- index.js
+++ index.js
@@ -9,39 +9,43 @@ var PHANTOM_ARGUMENTS = ["loadImages", "diskCache", "cookiesFile"];
 module.exports = function (options) {
 	options = _.extend({}, options || {});
 
-	var args = [];
-
-	_(options).pick(PHANTOM_ARGUMENTS).forOwn(function (key, value) {
-		args.push("--" + S(key).dasherize().toString());
-		args.push(value);
-	});
-
-	_.forOwn([].concat(options.tasks || {}), function (file, options) {
-		args.push("-t");
-		args.push(file + path.delimiter + JSON.stringify(options));
-	});
-
-	_([].concat(options.inject || [])).forIn(function (file) {
-		args.push("-i");
-		args.push(file);
-	});
-
-	if (options.parallel) {
-		args.push("-p");
-	}
-
-	args.push(script);
-
 	return function (src, callback) {
 		var phantomjs = require("phantomjs").path;
+		var args = [];
+
+		_(options).pick(PHANTOM_ARGUMENTS).forOwn(function (value, key) {
+			args.push("--" + S(key).dasherize().toString());
+			args.push(value);
+		});
+		
+		args.push(script);
+
+		if (options.parallel) {
+			args.push("-p");
+		}
+
+		_.forOwn(options.tasks || {}, function (options, file) {
+			args.push("-t");
+			args.push(file + path.delimiter + JSON.stringify(options));
+		});
+
+		_([].concat(options.inject || [])).forIn(function (file) {
+			args.push("-i");
+			args.push(file);
+		});
+
+		args.push(src);
+
 		var spawn = require("child_process").spawn;
-		var p = spawn(phantomjs, args.concat(src));
+		var p = spawn(phantomjs, args);
 		p.on("exit", function (code) {
 			var error;
 			if (code !== 0) {
 				error = new Error("Phantomjs exited with code: " + code);
 			}
-			callback(error);
+			if (typeof callback === "function") {
+				callback(error);
+			}
 		});
 		return p;
 	};
