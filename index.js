3cc9b600e51714edaa9a534848d662848100c9c7
Alexandros Sigalas <alxarch@gmail.com>
2014-02-11 02:30:13 +0200

    Pass options to each task by JSON.stringify

--- index.js
+++ index.js
@@ -1,45 +1,48 @@
+var S = require("string");
 var _ = require("lodash");
-var phantomjs = require("phantomjs").path;
-var spawn = require("child_process").spawn;
-var arrayify = require("./lib/utils").arrayify;
-var path = require("path");
 
+var path = require("path");
 var script = path.resolve(__dirname, 'lib/task.js');
 
-module.exports = function (src, options, callback) {
-	options = _.extend({}, options || {});
-	var inject = arrayify(options.inject);
-	var tasks = _.flatten(arrayify(options.tasks));
-	var jobs = [];
-	var phatomargs = [
-		options.images ? '--load-images=' + !!options.images : null,
-		options.cookies ? '--cookies-file=' + options.cookies : null,
-		options.cache ? '--disk-cache=' + !!options.cache : null
-	].filter(_.ident);
+var PHANTOM_ARGUMENTS = ["loadImages", "diskCache", "cookiesFile"];
 
-	var args = [].concat(options, script, src);
-	if (options.outpus) {
-		args.push('--output');
-		args.push(options.output);
-	}
+module.exports = function (options) {
+	options = _.extend({}, options || {});
+
+	var args = [];
+
+	_(options).pick(PHANTOM_ARGUMENTS).forOwn(function (key, value) {
+		args.push("--" + S(key).dasherize().toString());
+		args.push(value);
+	});
+
+	_.forOwn([].concat(options.tasks || {}), function (file, options) {
+		args.push("-t");
+		args.push(file + path.delimiter + JSON.stringify(options));
+	});
+
+	_([].concat(options.inject || [])).forIn(function (file) {
+		args.push("-i");
+		args.push(file);
+	});
+
 	if (options.parallel) {
-		args.push('--parallel');
+		args.push("-p");
 	}
-	inject.forEach( function (i) {
-		args.push("--inject");
-		args.push(i);
-	});
-	tasks.forEach( function (t) {
-		args.push("--task");
-		args.push(t);
-	});
-	var p = spawn(phantomjs, args);
-	p.on("exit", function (code) {
-		var error;
-		if (code !== 0) {
-			error = new Error("Phantomjs exited with code: " + code);
-		}
-		callback(error);
-	});
-	return p;
+
+	args.push(script);
+
+	return function (src, callback) {
+		var phantomjs = require("phantomjs").path;
+		var spawn = require("child_process").spawn;
+		var p = spawn(phantomjs, args.concat(src));
+		p.on("exit", function (code) {
+			var error;
+			if (code !== 0) {
+				error = new Error("Phantomjs exited with code: " + code);
+			}
+			callback(error);
+		});
+		return p;
+	};
 };
